1c11a2f7255460ea7e4496b145e58c1804a9574b
Dhairya Khale <gullu822@gmail.com>
2022-07-28 04:25:39 +0530

    Step 1 completed

---
+++ CodeForces/ITMO/Binary Search/1b.cpp
@@ -0,0 +1,57 @@
+
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+
+using namespace std;
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	int n; cin>>n;
+	int T; cin>>T;
+
+	int arr[n];
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+
+	int res[T];
+	// cin.ignore(); must be there when using getline(cin, s)
+	int Tmain=T;
+	while(T--)
+	{
+		int x; cin>>x;
+
+		int l=-1,r=n;
+
+		while(l+1<r){
+			int m = l+(r-l)/2;
+			
+			if(arr[m]<=x)
+				l=m;
+			else
+				r=m;
+		}
+
+		cout<<l+1<<endl;
+	}
+	return 0;
+}
